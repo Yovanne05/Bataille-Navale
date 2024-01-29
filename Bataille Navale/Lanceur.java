07244509edb5d81494d46e6dd13459fed59cdab1
Yovanne05 <156245534+Yovanne05@users.noreply.github.com>
2024-01-29 19:42:29 +0100

    Add files via upload

---
+++ Bataille Navale/Lanceur.java
@@ -0,0 +1,104 @@
+public class Lanceur {
+
+    public static void main(String[] args) {
+//        Menu.BatailleNavale();
+
+        char[][] plateauInfo1 = Methode.creationPlateau();
+        char[][] plateauInfo2 = Methode.creationPlateau();
+
+        char[][] plateauAttaque1 = Methode.creationPlateau();
+        char[][] plateauAttaque2 = Methode.creationPlateau();
+
+//        Test 1, Fin de partie
+//        Methode.placementBateauX(plateauInfo1, 0, 0, 4, 'P');
+//        Methode.placementBateauX(plateauInfo1, 1, 0, 3, 'C');
+//        Methode.placementBateauX(plateauInfo1, 2, 0, 2, 'T');
+//        Methode.placementBateauX(plateauInfo1, 3, 0, 2, 'S');
+//        Methode.placementBateauX(plateauInfo1, 4, 0, 1, 'D');
+//
+//        Methode.placementBateauX(plateauInfo2, 0, 0, 4, 'P');
+//        Methode.placementBateauX(plateauInfo2, 1, 0, 3, 'C');
+//        Methode.placementBateauX(plateauInfo2, 2, 0, 2, 'T');
+//        Methode.placementBateauX(plateauInfo2, 3, 0, 2, 'S');
+//        Methode.placementBateauX(plateauInfo2, 4, 0, 1, 'D');
+//
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,0);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,1);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,2);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,3);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,4);
+//
+//        Methode.tir(plateauInfo1, plateauAttaque1, 1,0);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 1,1);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 1,2);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 1,3);
+//
+//        Methode.tir(plateauInfo1, plateauAttaque1, 2,0);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 2,1);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 2,2);
+//
+//        Methode.tir(plateauInfo1, plateauAttaque1, 3,0);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 3,1);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 3,2);
+//
+//        Methode.tir(plateauInfo1, plateauAttaque1, 4,0);
+//
+//
+//        Methode.deroulementPartieJvsJ(plateauInfo1, plateauInfo1, plateauAttaque1, plateauAttaque2, "J1", "J2");
+
+//        Test 2, Superposer des bateaux et en supprimer
+//        Methode.placementBateau(plateauInfo1,"J");
+
+
+//        Test 3  Tir succesif, et tir à l'eau
+//        Methode.placementBateauX(plateauInfo1, 0, 0, 4, 'P');
+//        Methode.placementBateauX(plateauInfo1, 1, 0, 3, 'C');
+//        Methode.placementBateauX(plateauInfo1, 2, 0, 2, 'T');
+//        Methode.placementBateauX(plateauInfo1, 3, 0, 2, 'S');
+//        Methode.placementBateauX(plateauInfo1, 4, 0, 1, 'D');
+//
+//        Methode.placementBateauX(plateauInfo2, 0, 0, 4, 'P');
+//        Methode.placementBateauX(plateauInfo2, 1, 0, 3, 'C');
+//        Methode.placementBateauX(plateauInfo2, 2, 0, 2, 'T');
+//        Methode.placementBateauX(plateauInfo2, 3, 0, 2, 'S');
+//        Methode.placementBateauX(plateauInfo2, 4, 0, 1, 'D');
+//
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,0);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,1);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,2);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,3);
+//        Methode.tir(plateauInfo1, plateauAttaque1, 0,4);
+//
+//        Methode.deroulementPartieJvsJ(plateauInfo1, plateauInfo1, plateauAttaque1, plateauAttaque2, "J1", "J2");
+
+//        Test 4, placement Aleatoire
+//        for (int i=0; i<4; i++){
+//            Methode.placementBatIA(plateauInfo1);
+//            Methode.afficherPlateauPourUtilisateur(plateauInfo1);
+//            Methode.espace(5);
+//            plateauInfo1 = Methode.creationPlateau();
+//        }
+
+//        Test 5, TirAI
+//        Methode.placementBateauX(plateauInfo1, 0, 0, 4, 'P');
+//        Methode.placementBateauX(plateauInfo1, 1, 0, 3, 'C');
+//        Methode.placementBateauX(plateauInfo1, 2, 0, 2, 'T');
+//        Methode.placementBateauX(plateauInfo1, 3, 0, 2, 'S');
+//        Methode.placementBateauX(plateauInfo1, 4, 0, 1, 'D');
+//
+//        Methode.placementBateauX(plateauInfo2, 0, 0, 4, 'P');
+//        Methode.placementBateauX(plateauInfo2, 1, 0, 3, 'C');
+//        Methode.placementBateauX(plateauInfo2, 2, 0, 2, 'T');
+//        Methode.placementBateauX(plateauInfo2, 3, 0, 2, 'S');
+//        Methode.placementBateauX(plateauInfo2, 4, 0, 1, 'D');
+//
+//        Methode.tourIA(plateauInfo1, plateauAttaque2);
+//
+//        Methode.afficherPlateau(plateauAttaque2);
+
+
+
+
+
+    }
+}
